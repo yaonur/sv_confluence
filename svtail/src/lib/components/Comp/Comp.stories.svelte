8e344ed1632c29945bedbb470f154ce52469c158
ynr <ya_onur@hotmail.com>
2023-05-18 01:06:24 +0300

    ss

---
+++ svtail/src/lib/components/Comp/Comp.stories.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { Meta, Story, Template } from '@storybook/addon-svelte-csf';
+	import Comp from './Comp.svelte';
+</script>
+
+<Meta title="ui/Comp" component={Comp} />
+
+<Template let:args>
+	<Comp {...args} />
+</Template>
+
+<Story name="John" args={{ user: 'john doe' }} />
+
+<Story name="Jane" source args={{ user: 'jane doe' }} />
--- svtail/src/routes/+page.svelte
+++ svtail/src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script lang='ts'> 
+	import {Comp} from "$comp/Comp";
 import Meow from "$comp/meow/Meow.svelte";
 </script>
 <button class="text-white px-4 sm:px-8 py-2 sm:py-3 bg-sky-700 hover:bg-sky-800"> ... </button>
@@ -6,7 +7,7 @@ import Meow from "$comp/meow/Meow.svelte";
 <!-- After -->
 <button class="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3"> ... </button>
 
-<Meow/>
+<Comp user="jane"/>
 
 
 
